9ba6a3e8e002860d9d9c782392abf2fe7d54f92a
IVoynelovych <121500133+IVoynelovych@users.noreply.github.com>
2024-01-13 11:21:14 +0000

    Deploying to gh-pages from @ IVoynelovych/TRAP@5aab0d9096d4adaa52703347804be24ec64341ba 🚀

---
+++ sessons_active.9f3f276f.js
@@ -0,0 +1,2 @@
+!function(){var o={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")};function e(){o.modal.classList.toggle("is-hidden"),document.body.classList.toggle("no-scroll")}console.log(o),o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
+//# sourceMappingURL=sessons_active.9f3f276f.js.map
